21be9b91fee36d56090db6b59d910a9be306feae
Seth Myers <smyer011@odu.edu>
2026-01-23 19:29:40 -0500

    forgot to save a file oops

--- training/train.py
+++ training/train.py
@@ -6,4 +6,4 @@ device = torch.accelerator.current_accelerator().type if torch.accelerator.is_av
 print(f"Using device: {device}")
 
 model = CNNet(window_size=10, num_joints=15, num_class=50, drop_prob=0.5).to(device)
-print(model)
+
